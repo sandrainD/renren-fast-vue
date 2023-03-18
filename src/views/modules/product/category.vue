b58ccec406f5e6fce20a9a86f4d1eb55206bf59f
sandrainD <j2568095536@gmail.com>
2023-03-18 14:04:26 +0800

    first commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,39 @@
+<template>
+<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus(){
+      this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+        }).then(({data}) => {
+          // 大括号解构之后拿到data中的data列表
+          console.log("success获取菜单数据",data.data)
+        })
+    }
+  },
+  created(){
+    this.getMenus();
+  }
+};
+
+</script>
+
+
+<style >
+</style>
