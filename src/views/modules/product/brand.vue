b2933a1b619719296945dd02e140a60d9b2e89b5
sandrainD <j2568095536@gmail.com>
2023-12-14 13:04:09 +0800

    品牌管理中的关联分类功能

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -12,7 +12,7 @@
       </el-form-item>
     </el-form>
     <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle"
-      style="width: 100%">
+      style="width: 100%;">
       <el-table-column type="selection" header-align="center" align="center" width="50">
       </el-table-column>
       <el-table-column prop="brandId" header-align="center" align="center" label="品牌id">
@@ -28,20 +28,18 @@
           <img :src="scope.row.logo" style="width: 100px; height: 80px" />
         </template>
       </el-table-column>
-      <el-table-column prop="descript" header-align="center" align="center" label="介绍">
-      </el-table-column>
-      <el-table-column prop="showStatus" header-align="center" align="center" label="显示状态[0-不显示；1-显示]">
+      <el-table-column prop="descript" header-align="center" align="center" label="介绍"></el-table-column>
+      <el-table-column prop="showStatus" header-align="center" align="center" label="显示状态">
         <template slot-scope="scope">
           <el-switch v-model="scope.row.showStatus" active-color="#13ce66" inactive-color="#ff4949" :active-value="1"
             :inactive-value="0" @change="updateBrandStatus(scope.row)"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column prop="firstLetter" header-align="center" align="center" label="检索首字母">
-      </el-table-column>
-      <el-table-column prop="sort" header-align="center" align="center" label="排序">
-      </el-table-column>
-      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+      <el-table-column prop="firstLetter" header-align="center" align="center" label="检索首字母"></el-table-column>
+      <el-table-column prop="sort" header-align="center" align="center" label="排序"></el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="250" label="操作">
         <template slot-scope="scope">
+          <el-button type="text" size="small" @click="updateCatelogHandle(scope.row.brandId)">关联分类</el-button>
           <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.brandId)">修改</el-button>
           <el-button type="text" size="small" @click="deleteHandle(scope.row.brandId)">删除</el-button>
         </template>
@@ -49,37 +47,105 @@
     </el-table>
     <el-pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" :current-page="pageIndex"
       :page-sizes="[10, 20, 50, 100]" :page-size="pageSize" :total="totalPage"
-      layout="total, sizes, prev, pager, next, jumper">
-    </el-pagination>
+      layout="total, sizes, prev, pager, next, jumper"></el-pagination>
     <!-- 弹窗, 新增 / 修改 -->
     <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+
+    <el-dialog title="关联分类" :visible.sync="cateRelationDialogVisible" width="30%">
+      <el-popover placement="right-end" v-model="popCatelogSelectVisible">
+        <category-cascader :catelogPath.sync="catelogPath"></category-cascader>
+        <div style="text-align: right; margin: 0">
+          <el-button size="mini" type="text" @click="popCatelogSelectVisible = false">取消</el-button>
+          <el-button type="primary" size="mini" @click="addCatelogSelect">确定</el-button>
+        </div>
+        <el-button slot="reference">新增关联</el-button>
+      </el-popover>
+      <el-table :data="cateRelationTableData" style="width: 100%">
+        <el-table-column prop="id" label="#"></el-table-column>
+        <el-table-column prop="brandName" label="品牌名"></el-table-column>
+        <el-table-column prop="catelogName" label="分类名"></el-table-column>
+        <el-table-column fixed="right" header-align="center" align="center" label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" size="small"
+              @click="deleteCateRelationHandle(scope.row.id, scope.row.brandId)">移除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cateRelationDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="cateRelationDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import AddOrUpdate from "./brand-add-or-update";
+import CategoryCascader from "../common/category-cascader";
 export default {
   data() {
     return {
       dataForm: {
-        key: "",
+        key: ""
       },
+      brandId: 0,
+      catelogPath: [],
       dataList: [],
+      cateRelationTableData: [],
       pageIndex: 1,
       pageSize: 10,
       totalPage: 0,
       dataListLoading: false,
       dataListSelections: [],
       addOrUpdateVisible: false,
+      cateRelationDialogVisible: false,
+      popCatelogSelectVisible: false
     };
   },
   components: {
     AddOrUpdate,
+    CategoryCascader
   },
   activated() {
     this.getDataList();
   },
   methods: {
+    addCatelogSelect() {
+      //{"brandId":1,"catelogId":2}
+      this.popCatelogSelectVisible = false;
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/save"),
+        method: "post",
+        data: this.$http.adornData({ brandId: this.brandId, catelogId: this.catelogPath[this.catelogPath.length - 1] }, false)
+      }).then(({ data }) => {
+        this.getCateRelation();
+      });
+    },
+    deleteCateRelationHandle(id, brandId) {
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/delete"),
+        method: "post",
+        data: this.$http.adornData([id], false)
+      }).then(({ data }) => {
+        this.getCateRelation();
+      });
+    },
+    updateCatelogHandle(brandId) {
+      this.cateRelationDialogVisible = true;
+      this.brandId = brandId;
+      this.getCateRelation();
+    },
+    getCateRelation() {
+      this.$http({
+        url: this.$http.adornUrl("/product/categorybrandrelation/catelog/list"),
+        method: "get",
+        params: this.$http.adornParams({
+          brandId: this.brandId
+        })
+      }).then(({ data }) => {
+        this.cateRelationTableData = data.data;
+      });
+    },
     // 获取数据列表
     getDataList() {
       this.dataListLoading = true;
@@ -105,15 +171,16 @@ export default {
     updateBrandStatus(data) {
       console.log("最新信息", data);
       let { brandId, showStatus } = data;
+      //发送请求修改状态
       this.$http({
-        url: this.$http.adornUrl('/product/brand/update'),
-        method: 'post',
+        url: this.$http.adornUrl("/product/brand/update"),
+        method: "post",
         data: this.$http.adornData({ brandId, showStatus }, false)
       }).then(({ data }) => {
         this.$message({
           type: "success",
-          message: "状态修改成功"
-        })
+          message: "状态更新成功"
+        });
       });
     },
     // 每页数
@@ -142,7 +209,7 @@ export default {
     deleteHandle(id) {
       var ids = id
         ? [id]
-        : this.dataListSelections.map((item) => {
+        : this.dataListSelections.map(item => {
           return item.brandId;
         });
       this.$confirm(
@@ -151,13 +218,13 @@ export default {
         {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
-          type: "warning",
+          type: "warning"
         }
       ).then(() => {
         this.$http({
           url: this.$http.adornUrl("/product/brand/delete"),
           method: "post",
-          data: this.$http.adornData(ids, false),
+          data: this.$http.adornData(ids, false)
         }).then(({ data }) => {
           if (data && data.code === 0) {
             this.$message({
@@ -166,14 +233,14 @@ export default {
               duration: 1500,
               onClose: () => {
                 this.getDataList();
-              },
+              }
             });
           } else {
             this.$message.error(data.msg);
           }
         });
       });
-    },
-  },
+    }
+  }
 };
 </script>
