7fcf57b7dd9ebea5f856afb0ee9af2bb12657f3e
liutong <ltsttl33@gmail.com>
2024-04-13 08:53:55 +0800

    message

--- src/router/index.js
+++ src/router/index.js
@@ -154,6 +154,7 @@ function fnAddDynamicMenuRoutes (menuList = [], routes = []) {
     console.log(mainRoutes.children)
     console.log('%c!<-------------------- 动态(菜单)路由 e -------------------->', 'color:blue')
   }
+
 }
 
 export default router
